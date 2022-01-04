1ea1e9d69ef0e47e327634120159c56dd4bf2030
Poojashree-k05 <poojashree.k@collabera.com>
2022-01-04 18:08:03 +0530

    BmwCases.java

--- java-app/src/test/java/clsobj/Bmw.java
+++ java-app/src/test/java/clsobj/Bmw.java
@@ -1,4 +1,8 @@
 package clsobj;
 
-public class Bmw extends Car{
+// bmw is a car
+// -> object of bmw should be object of car
+// -> object of car is not the object of bmw
+public class Bmw extends Car {
+
 }
