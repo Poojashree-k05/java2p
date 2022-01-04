5769dfdd7ad30ea57d5ad24a59ec075014c1bd0b
Poojashree-k05 <poojashree.k@collabera.com>
2022-01-04 12:39:17 +0530

    javaprogram1

---
+++ java-app/src/test/java/AdditionTest.java
@@ -0,0 +1,5 @@
+public class AdditionTest {
+    public void add(int num1, int num2){
+
+    }
+}
---
+++ java-app/src/test/java/JavaBasicTest.java
@@ -0,0 +1,17 @@
+public class JavaBasicTest {
+    public void variables(){
+        int age=10;
+        float flt=20.6f;
+        double dbl=56.56;
+        char ch='a';
+        boolean isManager=false;
+        byte byt=10;
+        short sh=10;
+        long lng=45464646464646L;
+        String nm="android";
+    }
+
+    public void testMethods(){
+
+    }
+}
