eb5ab1ef18c6b406ec5fec1600afbfdafbd29b34
Poojashree-k05 <poojashree.k@collabera.com>
2022-01-12 11:32:35 +0530

    creating basic rest app

---
+++ Sampledemo/src/test/java/com/ani/Sampledemo/SampledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.Sampledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
