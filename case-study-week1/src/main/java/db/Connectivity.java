c1f08b6eee2ec70fc3402c944e3ef9d03d7683f8
Poojashree-k05 <poojashree.k@collabera.com>
2022-01-24 15:01:58 +0530

    case study week1

--- case-study-week1/src/main/java/db/Connectivity.java
+++ case-study-week1/src/main/java/db/Connectivity.java
@@ -1,29 +1,22 @@
 package db;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.cj.jdbc.Driver;
-
 public class Connectivity {
-
     private final Connection connection;
 
-    public Connectivity() throws SQLException {
-
-        new Driver(); // initialize the mysql driver
+    public Connectivity() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/week1_case_study_db",
+                "jdbc:mysql://localhost:3306/java_training_db",
                 "root",
                 "password"
-        ); // trying to make a connectivity
-
-        connection.setAutoCommit(false); // enabling txn management
+        );
+        connection.setAutoCommit(false);
         System.out.println("---- Connected to MySQL Successfully ----");
     }
-
     public Connection getConnection() {
         return connection;
     }
